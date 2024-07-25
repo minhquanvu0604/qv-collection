2b689cbb92099ed1c7624aa389070c7355631b99
MinhQuanVu <quanvu0604@gmail.com>
2024-07-25 14:26:52 +1000

    little update

--- docker/zed/zed-ros2-wrapper-vanilla/build_image.sh
+++ docker/zed/zed-ros2-wrapper-vanilla/build_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t zed_ros2_from_cuda_image \
+docker build -t zed_ros2_vanilla_image \
     -f ./Dockerfile.desktop-humble .
--- docker/zed/zed-ros2-wrapper-vanilla/run_image.sh
+++ docker/zed/zed-ros2-wrapper-vanilla/run_image.sh
@@ -3,7 +3,7 @@
 #   zed_ros2_desktop_image:latest
 docker run --gpus=all -it --privileged --ipc=host --pid=host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY \
   -v /dev:/dev -v /tmp/.X11-unix/:/tmp/.X11-unix \
-  zed_ros2_desktop_image:latest
+  zed_ros2_vanilla_image:latest
 
 
 #-v ${HOME}/zed_docker_ai/:/usr/local/zed/resources/ \
