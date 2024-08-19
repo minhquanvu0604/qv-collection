c9a771036988003e8db9c4bdad60bc187e515a0b
Quan Vu <quanvu0604@gmail.com>
2024-08-20 01:13:18 +1000

    update run_image.ps1 for wool

--- docker/wool-estimation/run_container.ps1
+++ docker/wool-estimation/run_container.ps1
@@ -4,10 +4,12 @@ docker run -it `
     --detach `
     --network host `
     --ipc=host `
-    --name wool_container_extra `
+    --name wool_container `
     --gpus all `
-    -v "C:\PERSONAL DATA ROG\git\wool_diameter\wool-diameter:/root/wool_estimation" `
-    -v "C:\PERSONAL DATA ROG\uts\AWI:/root/wool_estimation/fibre_segmentation/data" `
+    -v "C:\PERSONAL DATA ROG\git\wool_diameter\wool-diameter:/root/wool-estimation" `
+    -v "C:\PERSONAL DATA ROG\uts\AWI:/root/AWI" `
     -e DISPLAY=host.docker.internal:0.0 `
-    --workdir /root/wool_estimation `
-    wool_image
+    --workdir /root/wool-estimation `
+    minhquanvu0604/wool_image:first_trained1.0
+
+# -v "C:\PERSONAL DATA ROG\uts\AWI:/root/wool_estimation/fibre_segmentation/data" `
